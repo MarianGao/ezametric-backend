9ef1a20fb1b6f54714e5d3cf11feea35edf109f8
MarianGao <marianyuzuninomi@gmail.com>
2025-07-13 08:04:58 +1000

    Initial C# API commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ezametric_backend;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HelloController : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+    public IActionResult Get()
+    {
+        return Ok("Hello World from C# API!");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "EzaMetric Backend API",
+        Description = "A simple ASP.NET Core Web API for EzaMetric - Hello World Test",
+        Contact = new OpenApiContact
+        {
+            Name = "EzaMetric Team"
+        }
+    });
+});
+
+// Add CORS support
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "EzaMetric Backend API v1");
+        options.RoutePrefix = string.Empty;
+        options.DocumentTitle = "EzaMetric API Documentation";
+    });
+}
+
+// Use CORS
+app.UseCors("AllowAll");
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
